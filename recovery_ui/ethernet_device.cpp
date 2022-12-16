4f805e3746482979efc4092a84640cd954f32b86
DroidFreak32 <rushabshah32@gmail.com>
2022-12-16 10:27:30 +0000

    Trying to enable ethernet
    Change-Id: Ice6c04ed9b526ea60636ef65b5ac68167cb89bdc

--- recovery_ui/ethernet_device.cpp
+++ recovery_ui/ethernet_device.cpp
@@ -46,9 +46,14 @@ void EthernetDevice::PreRecovery() {
 
 void EthernetDevice::PreFastboot() {
   android::base::SetProperty("fastbootd.protocol", "tcp");
-
+  struct ifaddrs* ifaddr;
+  getifaddrs(&ifaddr);
   if (SetInterfaceFlags(IFF_UP, 0) < 0) {
-    LOG(ERROR) << "Failed to bring up interface";
+    std::unique_ptr<struct ifaddrs, decltype(&freeifaddrs)> guard{ ifaddr, freeifaddrs };
+      for (struct ifaddrs* ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next) {
+    PLOG(ERROR) << "Detected interface " << ifa->ifa_name;
+  }
+    LOG(ERROR) << "Failed to bring up interface ";
     return;
   }
 
@@ -96,6 +101,7 @@ void EthernetDevice::SetTitleIPv6LinkLocalAddress(const bool interface_up) {
 
   std::unique_ptr<struct ifaddrs, decltype(&freeifaddrs)> guard{ ifaddr, freeifaddrs };
   for (struct ifaddrs* ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next) {
+    PLOG(ERROR) << "Detected interface " << ifa->ifa_name;
     if (ifa->ifa_addr->sa_family != AF_INET6 || interface != ifa->ifa_name) {
       continue;
     }
